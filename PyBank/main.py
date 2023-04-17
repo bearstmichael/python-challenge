acd40dd42940c5e1d35f4f5293261ddf607beac7
Bear St. Michael <128632655+bearstmichael@users.noreply.github.com>
2023-04-16 22:33:34 -0400

    done with PyPoll, figured out what's wrong wtih PyBank but haven't fixed it yet

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,6 +13,7 @@ running_total = 0
 changes = []
 this_month = 0
 last_month = 0
+new_list = []
 
 for row in csv_reader: #for every row in our dataset
     if month_count > 0: #if we're not in the first row of data
@@ -21,34 +22,37 @@ for row in csv_reader: #for every row in our dataset
     month_count += 1 #add 1 to the number of months we've looked at
     running_total += int(row[1]) #add this running total
     last_month = int(row[1]) #this month's total? Just remember that for now, next month I'll ask you what it is so that we can see what the change was.
+    new_list.append(row)
 
-running_changes_total=0
-greatest_increase = changes[0]
-greatest_decrease = changes[0]
+print(new_list)
 
-for change in changes: #for each of the values of a the list of successive differences
-    running_changes_total += change #add them up one at a time, in a running total
-    if change > greatest_increase: #if you find a change that's bigger than the so-far-biggest change
-        greatest_increase = change #...replace it as the biggest change
-    if change < greatest_decrease: #if you find a change that's smaller than the so-far-smallest change
-        greatest_decrease = change #...replace it as the smallest change
+# running_changes_total=0
+# greatest_increase = changes[0]
+# greatest_decrease = changes[0]
 
-average = round(running_changes_total/(month_count-1),2) #the average is that running total divided by the number of months (less one, b/c 86 months only have 85 changes from one month to the next)
+# for change in changes: #for each of the values of a the list of successive differences
+#     running_changes_total += change #add them up one at a time, in a running total
+#     if change > greatest_increase: #if you find a change that's bigger than the so-far-biggest change
+#         greatest_increase = change #...replace it as the biggest change
+#     if change < greatest_decrease: #if you find a change that's smaller than the so-far-smallest change
+#         greatest_decrease = change #...replace it as the smallest change
 
-print(f"Total Months: {month_count}")
-print(f"Total: ${running_total}")
-print(f"Average Change: ${average}")
+# average = round(running_changes_total/(month_count-1),2) #the average is that running total divided by the number of months (less one, b/c 86 months only have 85 changes from one month to the next)
+
+# print(f"Total Months: {month_count}")
+# print(f"Total: ${running_total}")
+# print(f"Average Change: ${average}")
 
 
-#for some reason, I'm having a hard time extracting the month that corresponds to the greatest inc/dec
-#I'm not sure why this doesn't work:
-greatest_month=""
-least_month=""
-for row in csv_reader:
-    if int(row[1]) == int(greatest_increase):
-        greatest_month = str(row[0])
-    if int(row[1]) == int(greatest_decrease):
-        least_month = str(row[0])
+# #for some reason, I'm having a hard time extracting the month that corresponds to the greatest inc/dec
+# #I'm not sure why this doesn't work:
+# greatest_month=""
+# least_month=""
+# for row in new_list:
+#     if int(row[1]) == int(greatest_increase):
+#         greatest_month = str(row[0])
+#     if int(row[1]) == int(greatest_decrease):
+#         least_month = str(row[0])
 
-print(f"Greatest Increase: {greatest_month} (${greatest_increase})")
-print(f"Greatest Decrease: {least_month} (${greatest_decrease})")
+# print(f"Greatest Increase: {greatest_month} (${greatest_increase})")
+# print(f"Greatest Decrease: {least_month} (${greatest_decrease})")
---
+++ PyPoll/main.py
@@ -0,0 +1,47 @@
+import os
+import csv
+
+path = 'Resources/election_data.csv' # Path to collect data from the Resources folder
+csv_file = open(path) #go down that path
+csv_reader = csv.reader(csv_file) #read the csv dataset there, turning it into an array (well, list techincally) that we can read here
+next(csv_file) #skip the first row of column headers
+
+vote_counter = 0 #set initial value of vote counter
+candidates = [] #set initial empty list of unique candidates
+csv_reader2 = [] #set initial empty list that I'm going to copy my csv dataset into so I can re-iterate through
+
+#this block makes the list of unique candidates, counts all the votes, and makes a duplicate of the csv so we can iterate through it again
+for row in csv_reader: #for every row in this dataset
+    vote_counter += 1 #add one to the counter, helping us count the total number of votes
+    if row[2] in candidates: #if the candidate in that row is already in our list of unique candidates
+        "" #do nothing
+    else:
+        candidates.append(row[2]) #but if they're aren't in the list already, put them in the list
+    csv_reader2.append(row) #and then copy that row into the new blank list
+
+total_votes_per_can = [] #this is going to be a list, parallel to the unique candidate list, counting how many votes that candidate has. So the nth element of this list will be the total number of votes that the nth person in the candidates list got
+winning_vote_count = 0 #initial value for how many votes the winner got
+
+print("Election Results")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(f"Total Votes: {vote_counter}")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+#this block prints out the candidates and the vote results
+for i in range(len(candidates)): #we're going to iterate through our dataset, looking at one candidate at a time.
+    candidate_votes = 0 #initial count of votes for that candidate
+    for row in csv_reader2: #for every row in the dataset
+        if row[2] == candidates[i]: #if the candidate in that row is the candidate we're looking at this time around
+            candidate_votes += 1 #add one vote for them
+    total_votes_per_can.append(candidate_votes) #keep doing that through the whole dataset. Once you get to the end, you've got all the votes, so you put that final vote count into the list of "final vote counts"
+    
+    if total_votes_per_can[i]>winning_vote_count: #if that candidate has more votes than the current most-votes-getter
+        winning_vote_count = total_votes_per_can[i] #make that number of votes the new "most votes"
+        winner = candidates[i] #and let that candidate become the current holder of the "winner title"
+    
+    percentage = round(100*int(total_votes_per_can[i])/vote_counter,3) #calc percentage of the total votes that that person got
+    print(f"{candidates[i]}: {percentage}% ({total_votes_per_can[i]})")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(f"Winner: {winner}")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~")
